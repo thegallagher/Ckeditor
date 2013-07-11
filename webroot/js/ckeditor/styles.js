8e379b492fbd79c52a66d82d5b880860254d2650
David Gallagher <david@thegallagher.net>
2013-07-11 16:33:52 +1000

    Change styles to bootstrap styles

--- webroot/js/ckeditor/styles.js
+++ webroot/js/ckeditor/styles.js
@@ -13,99 +13,122 @@
 // Otherwise it is strongly recommended to customize this file to match your
 // website requirements and design properly.
 
-CKEDITOR.stylesSet.add( 'default', [
+CKEDITOR.stylesSet.add( 'bootstrap', [
 	/* Block Styles */
 
-	// These styles are already available in the "Format" combo ("format" plugin),
-	// so they are not needed here by default. You may enable them to avoid
-	// placing the "Format" combo in the toolbar, maintaining the same features.
-	/*
-	{ name: 'Paragraph',		element: 'p' },
-	{ name: 'Heading 1',		element: 'h1' },
-	{ name: 'Heading 2',		element: 'h2' },
-	{ name: 'Heading 3',		element: 'h3' },
-	{ name: 'Heading 4',		element: 'h4' },
-	{ name: 'Heading 5',		element: 'h5' },
-	{ name: 'Heading 6',		element: 'h6' },
-	{ name: 'Preformatted Text',element: 'pre' },
-	{ name: 'Address',			element: 'address' },
-	*/
+	{ name: 'Lead Paragraph', element: 'p', attributes: { 'class': 'lead' } },
 
-	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
-	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
-	{
-		name: 'Special Container',
-		element: 'div',
-		styles: {
-			padding: '5px 10px',
-			background: '#eee',
-			border: '1px solid #ccc'
-		}
-	},
+	//{ name: 'Text Align Left', element: 'p', attributes: { 'class': 'text-left' } },
+	//{ name: 'Text Align Center', element: 'p', attributes: { 'class': 'text-center' } },
+	//{ name: 'Text Align Right', element: 'p', attributes: { 'class': 'text-right' } },
+
+	//{ name: 'Pull Right', element: 'blockquote', attributes: { 'class': 'pull-right' } },
+
 
 	/* Inline Styles */
 
-	// These are core styles available as toolbar buttons. You may opt enabling
-	// some of them in the Styles combo, removing them from the toolbar.
-	// (This requires the "stylescombo" plugin)
-	/*
-	{ name: 'Strong',			element: 'strong', overrides: 'b' },
-	{ name: 'Emphasis',			element: 'em'	, overrides: 'i' },
-	{ name: 'Underline',		element: 'u' },
-	{ name: 'Strikethrough',	element: 'strike' },
-	{ name: 'Subscript',		element: 'sub' },
-	{ name: 'Superscript',		element: 'sup' },
-	*/
+	{ name: 'Text Color Muted', element: 'span', attributes: { 'class': 'muted' } },
+	{ name: 'Text Color Warning', element: 'span', attributes: { 'class': 'text-warning' } },
+	{ name: 'Text Color Error', element: 'span', attributes: { 'class': 'text-error' } },
+	{ name: 'Text Color Info', element: 'span', attributes: { 'class': 'text-info' } },
+	{ name: 'Text Color Success', element: 'span', attributes: { 'class': 'text-success' } },
 
-	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+	{ name: 'Initialism', element: 'abbr', attributes: { 'class': 'initialism' } },
+	//{ name: 'Initialism', element: 'abbr', attributes: { 'class': 'initialism' } }
 
-	{ name: 'Big',				element: 'big' },
-	{ name: 'Small',			element: 'small' },
-	{ name: 'Typewriter',		element: 'tt' },
-
-	{ name: 'Computer Code',	element: 'code' },
-	{ name: 'Keyboard Phrase',	element: 'kbd' },
-	{ name: 'Sample Text',		element: 'samp' },
-	{ name: 'Variable',			element: 'var' },
-
-	{ name: 'Deleted Text',		element: 'del' },
-	{ name: 'Inserted Text',	element: 'ins' },
-
-	{ name: 'Cited Work',		element: 'cite' },
-	{ name: 'Inline Quotation',	element: 'q' },
-
-	{ name: 'Language: RTL',	element: 'span', attributes: { 'dir': 'rtl' } },
-	{ name: 'Language: LTR',	element: 'span', attributes: { 'dir': 'ltr' } },
 
 	/* Object Styles */
 
-	{
-		name: 'Styled image (left)',
-		element: 'img',
-		attributes: { 'class': 'left' }
-	},
+	{ name: 'Unstyled List', element: 'ul', attributes: { 'class': 'unstyled' } },
+	{ name: 'Horizontal Definition List', element: 'dl', attributes: { 'class': 'dl-horizontal' } },
 
-	{
-		name: 'Styled image (right)',
-		element: 'img',
-		attributes: { 'class': 'right' }
-	},
+	{ name: 'Table', element: 'table', attributes: { 'class': 'table' } },
+	{ name: 'Table Stripped', element: 'table', attributes: { 'class': 'table table-striped' } },
+	{ name: 'Table Bordered', element: 'table', attributes: { 'class': 'table table-bordered' } },
+	{ name: 'Table Hover', element: 'table', attributes: { 'class': 'table table-hover' } },
+	{ name: 'Table Condensed', element: 'table', attributes: { 'class': 'table table-condensed' } },
+	{ name: 'Table Row Success', element: 'tr', attributes: { 'class': 'success' } },
+	{ name: 'Table Row Error', element: 'tr', attributes: { 'class': 'error' } },
+	{ name: 'Table Row Warning', element: 'tr', attributes: { 'class': 'warning' } },
+	{ name: 'Table Row Info', element: 'tr', attributes: { 'class': 'info' } },
 
-	{
-		name: 'Compact table',
-		element: 'table',
-		attributes: {
-			cellpadding: '5',
-			cellspacing: '0',
-			border: '1',
-			bordercolor: '#ccc'
-		},
-		styles: {
-			'border-collapse': 'collapse'
-		}
-	},
+	/*
+	{ name: 'Form Search', element: 'form', attributes: { 'class': 'form-search' } },
+	{ name: 'Form Search Query', element: 'input', attributes: { 'class': 'search-query', 'type': 'search' } },
+	{ name: 'Form Inline', element: 'form', attributes: { 'class': 'form-inline' } },
+	{ name: 'Form Horizontal', element: 'form', attributes: { 'class': 'form-horizontal' } },
+	{ name: 'Form Control Group', element: 'div', attributes: { 'class': 'control-group' } },
+	{ name: 'Form Control Group Warning', element: 'div', attributes: { 'class': 'control-group warning' } },
+	{ name: 'Form Control Group Error', element: 'div', attributes: { 'class': 'control-group error' } },
+	{ name: 'Form Control Group Info', element: 'div', attributes: { 'class': 'control-group info' } },
+	{ name: 'Form Control Group Success', element: 'div', attributes: { 'class': 'control-group success' } },
+	{ name: 'Form Controls', element: 'div', attributes: { 'class': 'controls' } },
+	{ name: 'Form Controls Row', element: 'div', attributes: { 'class': 'controls controls-row' } },
+	{ name: 'Form Actions', element: 'div', attributes: { 'class': 'form-actions' } },
+	{ name: 'Form Label', element: 'label', attributes: { 'class': 'control-label' } },
+	{ name: 'Form Checkbox Inline', element: 'label', attributes: { 'class': 'checkbox inline' } },
+	{ name: 'Form Input Prepend', element: 'div', attributes: { 'class': 'input-prepend' } },
+	{ name: 'Form Input Append', element: 'div', attributes: { 'class': 'input-append' } },
+	{ name: 'Form Input Add-on', element: 'span', attributes: { 'class': 'add-on' } },
+	{ name: 'Form Input Size Block', element: 'input', attributes: { 'class': 'input-block-level' } },
+	{ name: 'Form Input Size Mini', element: 'input', attributes: { 'class': 'input-mini' } },
+	{ name: 'Form Input Size Small', element: 'input', attributes: { 'class': 'input-small' } },
+	{ name: 'Form Input Size Medium', element: 'input', attributes: { 'class': 'input-medium' } },
+	{ name: 'Form Input Size Large', element: 'input', attributes: { 'class': 'input-large' } },
+	{ name: 'Form Input Size X Large', element: 'input', attributes: { 'class': 'input-xlarge' } },
+	{ name: 'Form Input Size XX Large', element: 'input', attributes: { 'class': 'input-xxlarge' } },
+	{ name: 'Form Input Uneditable', element: 'span', attributes: { 'class': 'uneditable-input' } },
+	{ name: 'Form Help Inline', element: 'span', attributes: { 'class': 'help-inline' } },
+	{ name: 'Form Help Block', element: 'span', attributes: { 'class': 'help-block' } },
+	*/
 
-	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
-	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
+	{ name: 'Button', element: 'a', attributes: { 'class': 'btn' } },
+	{ name: 'Button Primary', element: 'a', attributes: { 'class': 'btn btn-primary' } },
+	{ name: 'Button Info', element: 'a', attributes: { 'class': 'btn btn-info' } },
+	{ name: 'Button Success', element: 'a', attributes: { 'class': 'btn btn-success' } },
+	{ name: 'Button Warning', element: 'a', attributes: { 'class': 'btn btn-warning' } },
+	{ name: 'Button Danger', element: 'a', attributes: { 'class': 'btn btn-danger' } },
+	{ name: 'Button Link', element: 'a', attributes: { 'class': 'btn btn-link' } },
+	{ name: 'Button Large', element: 'a', attributes: { 'class': 'btn btn-large' } },
+	{ name: 'Button Small', element: 'a', attributes: { 'class': 'btn btn-small' } },
+	{ name: 'Button Mini', element: 'a', attributes: { 'class': 'btn btn-mini' } },
+	{ name: 'Button Block', element: 'a', attributes: { 'class': 'btn btn-block' } },
+	{ name: 'Button Disabled', element: 'a', attributes: { 'class': 'btn disabled' } },
+	/*
+	{ name: 'Button Group', element: 'div', attributes: { 'class': 'btn-group' } },
+	{ name: 'Button Group Vertical', element: 'div', attributes: { 'class': 'btn-group btn-group-vertical' } },
+	{ name: 'Button Toolbar', element: 'div', attributes: { 'class': 'btn-toolbar' } },
+	{ name: 'Button Dropdown Toggle', element: 'a', attributes: { 'class': 'dropdown-toggle', 'data-toggle': 'dropdown' } },
+	*/
+
+	{ name: 'Image Rounded', element: 'img', attributes: { 'class': 'img-rounded' } },
+	{ name: 'Image Circle', element: 'img', attributes: { 'class': 'img-circle' } },
+	{ name: 'Image Polaroid', element: 'img', attributes: { 'class': 'img-polaroid' } },
+
+	/*
+	{ name: 'Menu Dropdown', element: 'div', attributes: { 'class': 'dropdown' } },
+	{ name: 'Menu Dropdown Menu', element: 'ul', attributes: { 'class': 'dropdown-menu' } },
+	{ name: 'Menu Item Submenu', element: 'li', attributes: { 'class': 'dropdown-submenu' } },
+	{ name: 'Menu Item Divider', element: 'li', attributes: { 'class': 'divider' } },
+	{ name: 'Menu Item Disabled', element: 'li', attributes: { 'class': 'disabled' } },
+	{ name: 'Menu Item Disabled', element: 'li', attributes: { 'class': 'disabled' } },
+	*/
+
+	{ name: 'Thumbnails', element: 'ul', attributes: { 'class': 'thumbnails' } },
+	{ name: 'Thumbnails Item', element: 'a', attributes: { 'class': 'thumbnail' } },
+
+	{ name: 'Label', element: 'span', attributes: { 'class': 'label' } },
+	{ name: 'Label Success', element: 'span', attributes: { 'class': 'label label-success' } },
+	{ name: 'Label Warning', element: 'span', attributes: { 'class': 'label label-warning' } },
+
+	{ name: 'Well', element: 'div', attributes: { 'class': 'well' } },
+	{ name: 'Well Large', element: 'div', attributes: { 'class': 'well well-large' } },
+	{ name: 'Well Small', element: 'div', attributes: { 'class': 'well well-small' } },
+
+	{ name: 'Pull Left', element: 'div', attributes: { 'class': 'pull-left' } },
+	{ name: 'Pull Right', element: 'div', attributes: { 'class': 'pull-right' } },
+	{ name: 'Clear Fix', element: 'div', attributes: { 'class': 'clearfix' } },
+
+
 ]);
 
